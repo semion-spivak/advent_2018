b9c6b1b73bd29beddce1062dd7de7461c40e1746
Semion Spivak <semion.spivak@gmail.com>
2018-12-21 11:46:48 +0200

    done day 5 pt 2

--- 05.py
+++ 05.py
@@ -1,32 +1,37 @@
-from itertools import chain
+from itertools import chain, filterfalse, islice
+from functools import partial
 
-def are_opposit_charge(a, b):
+def opposit_charge(a, b):
     res = abs(a - b) == 32
-    # print(a, b, res)
     return res
 
+def remove_letter(line, letter):
+    upper = ord(chr(letter).upper())
+    lower = ord(chr(letter).lower())
+    return list(filterfalse(lambda x: x in (upper, lower), line))
 
-
-with open('05.in', 'r') as f:
-    poly = list(map(ord, f.readline()))
-    # poly = list(map(ord, 'dabAcCaCBAcCcaDA'))
-    # print("".join(map(chr, poly)))
-    initial_len = len(poly)
-    
+def wrap(poly):
     no_more_matches = False
     while not no_more_matches:
         if len(poly) <= 1:
             break
         for i in range(0, len(poly)-1):
-            if are_opposit_charge(poly[i], poly[i+1]):
-                # print(i, i+1)
-                poly = list(chain(poly[:i], poly[i+2:]))
-                # print("".join(map(chr, poly)))
+            if opposit_charge(poly[i], poly[i+1]):
+                poly = list(chain(islice(poly, 0, i), islice(poly, i+2, None)))
                 break
         else:
             no_more_matches = True
-    # print("".join(map(chr, poly)))
-    print(f"part 1: {len(poly)}")
-    
-    
+    return poly
 
+with open('05.in', 'r') as f:
+    p = list(map(ord, f.readline()))
+
+wrapped = wrap(p)
+print(f"part 1: {len(wrapped)}")
+
+len_list = []
+for l in range(ord('a'), ord('z')+1):
+    current_poly = remove_letter(wrapped, l)
+    len_list.append(len(wrap(current_poly)))
+
+print(f"part 2: {min(len_list)}")
